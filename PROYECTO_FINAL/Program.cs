374a8c07601cd1cc7dc9293de911ed7a5bcb3e01
MINYETTY AJ <MINYETTY AJ@MINYETTYVZ>
2021-12-02 18:19:34 -0400

    Agregar archivos de proyecto.

---
+++ PROYECTO_FINAL/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROYECTO_FINAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("")
+        }
+    }
+}
