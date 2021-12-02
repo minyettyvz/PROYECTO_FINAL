236256962d58fd893f1b354341d801604baacc3c
joaquinminyetty@gmail.com <joaquinminyetty@gmail.com>
2021-12-02 18:40:07 -0400

    menu principal

--- PROYECTO_FINAL/Program.cs
+++ PROYECTO_FINAL/Program.cs
@@ -10,7 +10,28 @@ namespace PROYECTO_FINAL
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("klk2"); 
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("---------------------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("******************ALVARO JOAQUIN MINYETY 2021-0194*******************");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("---------------------------------------------------------------------");
+
+            Console.WriteLine("1:AGENDA");
+            Console.WriteLine("2:CONVERSOR DE UNIDADES");
+            Console.WriteLine("3:CALCULADORA");
+            Console.WriteLine("0:SALIR");
+            Console.WriteLine("----------------------------------------------------------------------");
+            Console.Write("************DIGITE SOLO EL NUMERO DE SU OPCION DESEADA**************:");
+
+
+
+
+
+
+
+
+            Console.ReadKey();
 
         }
     }
